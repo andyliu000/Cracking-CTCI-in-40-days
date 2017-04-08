9466bf35e3058b36a20559a13c19eaa2f8c54438
AndyLiu404 <martinlau.aplus@gmail.com>
2017-04-08 13:07:53 +0800

    Add files via upload

---
+++ Day1/Ch1_2_CheckPermutation.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Ch1_2_CheckPermutation {
+    public static void main(String[] args) {
+        String s1 = "abcdef";
+        String s2 = "bcadee";
+        System.out.println(isPermutation(s1, s2));
+    }
+    
+    public static boolean isPermutation(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() != s2.length()) {
+            return false;
+        }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            if (!map.containsKey(c)) {
+                return false;
+            } else if (map.containsKey(c) && map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        return map.isEmpty();            
+    }
+}
